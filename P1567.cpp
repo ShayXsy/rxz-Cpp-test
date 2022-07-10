70059bcbecbc4d626a2c84e16a6222c287089989
Shay <shayxu217@outlook.com>
2022-07-10 19:30:29 +0800

    P1567

--- P1567.cpp
+++ P1567.cpp
@@ -1,10 +1,11 @@
 #include <cstdio>
 #include <algorithm>
 
+int a[1000000];
+
 int main() {
     int n, cnt = 1, max = -999;
     scanf("%d", &n);
-    int a[n];
     for (int i = 0; i < n; i++)
         scanf("%d", &a[i]);
     for (int i = 0; i < n; i++) {
@@ -13,7 +14,6 @@ int main() {
             max = std::max({cnt, max});
         } else cnt = 1;
     }
-    max = std::max(cnt, max);
     printf("%d\n", max);
     return 0;
 }
